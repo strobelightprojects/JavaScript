37e02d4716079b9257bfaa2712593393fc00bdc8
strobelightprojects <mattstrobel17@gmail.com>
2024-01-27 16:58:18 -0700

    almost done

---
+++ README.md
@@ -0,0 +1,27 @@
+# Overview
+
+{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+{Provide a description of the software that you wrote to demonstrate the JavaScript language.}
+
+{Describe your purpose for writing this software.}
+
+
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+*Visual Studio Code *Git/Github *Css 3 *HTMl 5 *JavaScript *JQuerey 1.12.4.min - Libaray for JavaScript
+
+# Useful Websites
+
+- [JQuery used to download libaray and find information on how to use the library](https://jquery.com/)
+- [W3Schools used to learn the basics of JavaScript ](https://www.w3schools.com/js)
+
+# Future Work
+
+
+- Would like to expand on the currency exchange part, need to find a free library that can pull live rates. 
+- I could defintly improve colors of the website. I would like to change the background and font colors. 
+- I would like to improve the spacing of the diffrent parts of the website improve alignment
---
+++ project.js
@@ -0,0 +1,51 @@
+//I changed my plan to do the concurrency converter but after looking for a libary I decided to just break up the requirments into sepeate parts
+
+// fulfill the first requiremnt of displaying something to the screen
+//this is just basic multiplication didn't find a library I liked to pull exchange rates 
+const multiply = (number1, number2) => number1 * number2; //The multiplcation when converting currency
+
+const multiplyNumbers = () => {
+    let factor1 = parseFloat(document.querySelector('#factor1').value);     // Setting the value in Html
+    let factor2 = parseFloat(document.querySelector('#factor2').value);
+    let result = multiply(factor1, factor2);
+    document.querySelector('#product').value = result; }
+    document.querySelector('#exchange').addEventListener('click', multiplyNumbers); //gives functionality to the button
+
+//Recursion 
+const factorial = (n) => {      //factorial calculations using recursion
+    if (n === 0 || n === 1) {           
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+document.querySelector('#factorialButton').addEventListener('click', () => {  //gives functionality to the button
+    let number = parseFloat(document.querySelector('#number').value);// Setting the value in Html
+    let result = factorial(number);
+    document.querySelector('#factorialResult').textContent = `Factorial of ${number}: ${result}`;
+});
+
+//Native Array ES6 Functions
+const arrayOperations = () => {
+    const numbers = [1, 2, 3, 4, 5];
+
+    // Map
+    let mappedArray = numbers.map(num => num * 2); // performing the map operation
+    document.querySelector('#mappedArray').textContent = `Mapped Array: ${mappedArray}`; // Setting the value in Html
+
+    // Filter
+    let filteredArray = numbers.filter(num => num % 2 === 0); // performing the filter operation
+    document.querySelector('#filteredArray').textContent = `Filtered Array: ${filteredArray}`;// Setting the value in Html
+
+    // Reduce
+    let sum = numbers.reduce((acc, curr) => acc + curr, 0); //perfroming the Reduce operation
+    document.querySelector('#sum').textContent = `Sum of Array: ${sum}`;// Setting the value in Html
+}
+
+document.querySelector('#arrayButton').addEventListener('click', arrayOperations);//gives functionality to the button
+
+//Javascript libaray requirement using JQuerey and CSS Dom streach challenge
+var exchange =$("#ex");
+exchange.css({color: "blue"});
+
